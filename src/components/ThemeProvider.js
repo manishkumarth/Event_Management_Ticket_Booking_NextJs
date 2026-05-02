6e8145f4da920df67b7bd624985fdc75b488aa1e
manishkumar <manish12099@gmail.com>
2026-05-02 18:30:44 +0530

    added default theme dark

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -4,11 +4,11 @@ import { useState, useEffect, createContext } from "react";
 export const ThemeContext = createContext({ theme: "light", setTheme: () => { } });
 
 export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   // On mount, load theme from localStorage
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") || "light";
+    const savedTheme = localStorage.getItem("theme") || "dark";
     setTheme(savedTheme);
     document.documentElement.setAttribute("data-theme", savedTheme);
   }, []);
