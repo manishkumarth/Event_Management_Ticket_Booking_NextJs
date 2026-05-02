60528b9f9e82dab15787e9677c0b143472639a83
manishkumar <manish12099@gmail.com>
2026-05-02 09:02:49 +0530

    fix payment success suspense issue

--- src/app/payment-success/page.js
+++ src/app/payment-success/page.js
@@ -1,11 +1,57 @@
-// app/payment-success/page.js
+// // app/payment-success/page.js
+// "use client";
+
+// import { useEffect } from "react";
+// import { useSearchParams } from "next/navigation";
+// import Link from "next/link";
+
+// export default function PaymentSuccess() {
+//   const searchParams = useSearchParams();
+//   const orderId = searchParams.get("order_id");
+
+//   useEffect(() => {
+//     if (!orderId) return;
+
+//     const verify = async () => {
+//       const res = await fetch("/api/verify-payment", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ order_id: orderId }),
+//       });
+
+//       const data = await res.json();
+
+//       if (data.success) {
+//         console.log("Payment Verified ✅");
+//       } else {
+//         console.log("Payment Failed ❌");
+//       }
+//     };
+    
+
+//     verify();
+//   }, [orderId]);
+
+//   return (
+//     <>
+//     <h1>Payment success</h1>
+//     <Link href="/ticket">Go to Ticket Page</Link>
+    
+//     </>
+//   )
+// }
+
+
+
 "use client";
 
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 
-export default function PaymentSuccess() {
+function PaymentSuccessContent() {
   const searchParams = useSearchParams();
   const orderId = searchParams.get("order_id");
 
@@ -29,16 +75,22 @@ export default function PaymentSuccess() {
         console.log("Payment Failed ❌");
       }
     };
-    
 
     verify();
   }, [orderId]);
 
   return (
     <>
-    <h1>Payment success</h1>
-    <Link href="/ticket">Go to Ticket Page</Link>
-    
+      <h1>Payment success</h1>
+      <Link href="/ticket">Go to Ticket Page</Link>
     </>
-  )
+  );
+}
+
+export default function PaymentSuccess() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PaymentSuccessContent />
+    </Suspense>
+  );
 }
